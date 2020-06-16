79ec05405586f5e96a95a6c4affb8074a2cb23ba
Administrator <kyw0906@163.com>
2020-06-16 18:19:30 +0800

    bmi commit 2020/06/16

--- lib/bmi/bmi.dart
+++ lib/bmi/bmi.dart
@@ -9,9 +9,18 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+//      theme: ThemeData.dark().copyWith(
+//          primaryColor: Color(0xFF0A0E21),
+//          scaffoldBackgroundColor: Color(0xFF0A0E21)),
       theme: ThemeData.dark().copyWith(
-          primaryColor: Color(0xFF0A0E21),
-          scaffoldBackgroundColor: Color(0xFF0A0E21)),
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0XFF0A0E21),
+      ),
+
+//      theme: ThemeData(
+//          primaryColor: Color(0XFF0A0E21),
+//          scaffoldBackgroundColor: Color(0XFF0A0E21),
+//          accentColor: Colors.purple),
       home: InputPage(),
     );
   }
