01328c49c261e709b4c21c29a3ab6a8059c586bd
Administrator <kyw0906@163.com>
2020-07-15 18:38:27 +0800

    chat screen commit 2020/07/15

--- lib/flashchat/screens/chat_screen.dart
+++ lib/flashchat/screens/chat_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:light/flashchat/constants.dart';
 
 final _firestore = Firestore.instance;
+FirebaseUser loggedUser;
 
 class ChatScreen extends StatefulWidget {
   static const String id = "chat_screen";
@@ -14,7 +15,7 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final _auth = FirebaseAuth.instance;
-  FirebaseUser loggedUser;
+
   String messageText;
   final messageTextController = TextEditingController();
   @override
@@ -61,9 +62,8 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               icon: Icon(Icons.close),
               onPressed: () {
-                /*_auth.signOut();
-                Navigator.pop(context);*/
-                messageStream();
+                _auth.signOut();
+                Navigator.pop(context);
               }),
         ],
         title: Text('⚡️Chat'),
@@ -82,6 +82,7 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      autofocus: false,
                       controller: messageTextController,
                       onChanged: (value) {
                         messageText = value;
@@ -92,8 +93,14 @@ class _ChatScreenState extends State<ChatScreen> {
                   FlatButton(
                     onPressed: () {
                       messageTextController.clear();
-                      _firestore.collection('messages').add(
-                          {'text': messageText, 'sender': loggedUser.email});
+                      final time = DateTime.now();
+                      String formatTime = "${time.hour}:${time.minute}";
+                      _firestore.collection('messages').add({
+                        'text': messageText,
+                        'sender': loggedUser.email,
+                        'sendTime': time.toString(),
+                        'sendFormatTime': formatTime
+                      });
                       //Implement send functionality.
                     },
                     child: Text(
@@ -124,19 +131,28 @@ class MessageStream extends StatelessWidget {
             ),
           );
         }
-        final messages = snapshot.data.documents;
+        final messages = snapshot.data.documents.reversed;
         List<MessageBubble> messageBubbles = [];
         for (var message in messages) {
           final messageText = message.data['text'];
           final messageSender = message.data['sender'];
+          final messageDateTime = message.data['sendTime'];
+          final messageFormatTime = message.data['sendFormatTime'];
+          final currentUser = loggedUser.email;
+          //the message from the logger in user.
           final messageBubble = MessageBubble(
-            text: messageText,
-            sender: messageSender,
-          );
+              text: messageText,
+              sender: messageSender,
+              isMe: currentUser == messageSender,
+              messageDateTime: messageDateTime,
+              formatTime: messageFormatTime);
           messageBubbles.add(messageBubble);
+          messageBubbles.sort((a, b) => DateTime.parse(b.messageDateTime)
+              .compareTo(DateTime.parse(a.messageDateTime)));
         }
         return Expanded(
           child: ListView(
+            reverse: true,
             padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
             children: messageBubbles,
           ),
@@ -149,29 +165,56 @@ class MessageStream extends StatelessWidget {
 class MessageBubble extends StatelessWidget {
   final String sender;
   final String text;
-
-  const MessageBubble({Key key, this.sender, this.text}) : super(key: key);
+  final bool isMe;
+  final String messageDateTime;
+  final String formatTime;
+  MessageBubble(
+      {Key key,
+      this.sender,
+      this.text,
+      this.isMe,
+      this.messageDateTime,
+      this.formatTime})
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
+    print(messageDateTime);
+
     return Padding(
       padding: EdgeInsets.all(10.0),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
+        crossAxisAlignment:
+            isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
         children: <Widget>[
+          Center(
+            child: Text(formatTime,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 12.0, color: Colors.black38)),
+          ),
           Text(
             sender,
-            style: TextStyle(fontSize: 12.0, color: Colors.black45),
+            style: TextStyle(fontSize: 12.0, color: Colors.black54),
           ),
           Material(
-            borderRadius: BorderRadius.circular(30.0),
+            borderRadius: isMe
+                ? BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                  )
+                : BorderRadius.only(
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                    topRight: Radius.circular(30)),
             elevation: 5.0,
-            color: Colors.lightBlueAccent,
+            color: isMe ? Colors.lightBlueAccent : Colors.white,
             child: Padding(
               padding:
                   const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
               child: Text(
-                '$text from $sender',
-                style: TextStyle(fontSize: 50, color: Colors.white),
+                '$text',
+                style: TextStyle(
+                    fontSize: 15, color: isMe ? Colors.white : Colors.black54),
               ),
             ),
           ),
