83d3c3576587c6978e251c481e4ecefe2a84ecbe
Hei Pi <kyw0906@163.com>
2020-07-17 00:44:56 +0800

    todoey commit 2020/07/17

---
+++ lib/todoey/screens/task_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class TaskScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: Container(
+        padding: EdgeInsets.only(top: 60, left: 30, right: 30, bottom: 30),
+        child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              child: Icon(
+                Icons.list,
+                size: 30,
+              ),
+              backgroundColor: Colors.white,
+              radius: 30,
+            ),
+            Text('Todoey'),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/todoey/todomain.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'file:///D:/FlutterBasicStudy/lib/todoey/screens/task_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: TaskScreen(),
+    );
+  }
+}
